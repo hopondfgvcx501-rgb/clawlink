58b45e0552caa9b588536c7f1f9b1cd65e8a1c99
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL>
2026-07-13 15:00:36 +0530

    fix: added strict open-next config manually to bypass powershell error

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,1 +1,20 @@
-const config = { default: { buildCommand: "npx next build" } }; export default config;
+const config = {
+  default: {
+    override: {
+      wrapper: "cloudflare-node",
+      converter: "edge",
+      proxyExternalRequest: "fetch"
+    }
+  },
+  edgeExternals: ["node:crypto"],
+  middleware: {
+    external: true,
+    override: {
+      wrapper: "cloudflare-edge",
+      converter: "edge",
+      proxyExternalRequest: "fetch"
+    }
+  }
+};
+
+export default config;
